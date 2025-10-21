d6a3135b590d7f17e8750e40cc4cb17902cb7f9d
GuitarV <guitarwei8@gmail.com>
2025-10-21 15:56:24 +0800

    Update Auto BR-1.0.user.js

--- Auto BR-1.0.user.js
+++ Auto BR-1.0.user.js
@@ -10,8 +10,8 @@
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/select2/4.1.0-rc.0/js/select2.min.js
 // @resource     SELECT2_CSS https://cdnjs.cloudflare.com/ajax/libs/select2/4.1.0-rc.0/css/select2.min.css
-// @updateURL    https://drive.corp.amazon.com/documents/BJS%20DCEO/Auto%20BR-1.0.user.js
-// @downloadURL  https://drive.corp.amazon.com/documents/BJS%20DCEO/Auto%20BR-1.0.user.js
+// @updateURL    https://github.com/GuitarV/Auto-Blast-Radius/blob/main/Auto%20BR-1.0.user.js
+// @downloadURL  https://github.com/GuitarV/Auto-Blast-Radius/blob/main/Auto%20BR-1.0.user.js
 // ==/UserScript==
 
 (function() {
